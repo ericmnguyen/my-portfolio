95f2ba35f63608593593b47186d24c65861a9e18
Eric Nguyen <nhat.eric.nguyen@gmail.com>
2026-06-20 13:35:35 +1000

    minor fixes

--- src/containers/Projects/index.jsx
+++ src/containers/Projects/index.jsx
@@ -11,11 +11,11 @@ const Projects = () => {
       <h2 className='projects-page__title text-white font-mono italic text-3xl pl-24'>List of my mini apps</h2>
       <div className="projects-page__app-list grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-9 justify-items-center">
         <Card image={project1} title="My Porfolio" tags={['react', 'redux', 'tailwind', 'html', 'css']} link='https://github.com/ericmnguyen/my-portfolio' />
-        <Card image={HealthClinic} title="Mental Health Clinic" tags={['html', 'css', 'javascript', 'aws lambda', 'AI bot']} link='https://github.com/ericmnguyen/my-portfolio' />
-        <Card image={project1} title="EP Charity" tags={['redux', 'react', 'bootstrap']} link='https://github.com/ericmnguyen/ep-charity' />
-        <Card image={project1} title="Bookstore" tags={['redux', 'react', 'bootstrap']} link='https://github.com/ericmnguyen/bookstore' />
-        <Card image={project1} title="Shopcat" tags={['react', 'typescript', 'bootstrap', 'redux']} link='https://github.com/ericmnguyen/shopcat' />
-        <Card image={project1} title="Red Envelop" tags={['react native', 'bootstrap']} link='https://github.com/ericmnguyen/LixiApp' />
+        <Card image={HealthClinic} title="Mental Health Clinic - AI Powered" tags={['html', 'css', 'javascript', 'aws lambda', 'AI bot']} link='https://github.com/ericmnguyen/my-portfolio' />
+        <Card image={project1} title="EP Charity" tags={['html', 'css', 'javascript', 'php', 'bootstrap']} link='https://github.com/ericmnguyen/ep-charity' />
+        <Card image={project1} title="Bookstore" tags={['html', 'css', 'javascript', 'php', 'bootstrap']} link='https://github.com/ericmnguyen/bookstore' />
+        <Card image={project1} title="Shopcat" tags={['react', 'typescript', 'tailwind', 'redux', 'next.js']} link='https://github.com/ericmnguyen/shopcat' />
+        <Card image={project1} title="Red Envelop" tags={['javascript', 'react native', 'bootstrap']} link='https://github.com/ericmnguyen/LixiApp' />
       </div>
       <ScrollDownIcon />
     </div>
